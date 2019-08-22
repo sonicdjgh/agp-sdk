68174491540083d056dcce0ca5a02393e69f72d3
EGLS <sonicdj1987@gmail.com>
2019-08-22 11:01:27 +0800

    4.6.53

--- egls-android-game-sdk-4/sdk/EglsSdkDemo/src/main/java/com/egls/demo/MainActivity.java
+++ egls-android-game-sdk-4/sdk/EglsSdkDemo/src/main/java/com/egls/demo/MainActivity.java
@@ -5,10 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.egls.platform.components.AGPManager;
-import com.egls.platform.interfaces.AGPClientPayProcessListener;
-import com.egls.platform.interfaces.AGPInitProcessListener;
-import com.egls.platform.interfaces.AGPLoginProcessListener;
 import com.egls.support.base.Constants;
+import com.egls.support.interfaces.SDKActionHandler;
 import com.egls.support.utils.AppUtil;
 
 public class MainActivity extends Activity {
@@ -16,12 +14,91 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AGPManager.initSDK(this, AppUtil.getVersionName(this), new AGPInitProcessListener() {// SDK初始化回调
+        AGPManager.initSDK(this, AppUtil.getVersionName(this), new SDKActionHandler() {
 
             @Override
-            public void onInitProcess(int code, String msg) {
-                if (code == 0) {// 当SDK初始化成功后再做后续的事情
+            public void onHandleInit(int state, String message) {// SDK初始化的結果处理
+                if (state == Constants.SDK_STATE_SUCCESS) {
+                    // 初始化成功后的处理
+                } else {
+                    // 初始化失败后的处理
+                }
+            }
 
+            @Override
+            public void onHandleAgreement(boolean isAgree) {// SDK是否同意用户协议的結果处理
+                if (isAgree) {
+                    // 玩家选择同意协议后的处理
+                } else {
+                    // 玩家选择不同意协议后的处理
+                }
+            }
+
+            @Override
+            public void onHandleLogin(int state, String token, String uid, String accountType, String nickName) {// SDK登录的結果处理
+                switch (state) {
+                    case Constants.SDK_STATE_SUCCESS:// 登录成功后的处理
+                        // accountType = "0"时，表示游客账号登录
+                        // accountType = "1"时，表示EGLS账号登录
+                        // accountType = "2"时，表示Google账号登录
+                        // accountType = "3"时，表示Facebook账号登录
+                        break;
+                    case Constants.SDK_STATE_CANCEL:// 登录取消后的处理
+                        break;
+                    case Constants.SDK_STATE_ERROR:// 登录失败后的处理
+                        break;
+                }
+            }
+
+            @Override
+            public void onHandleGuestBind(int state, String accountType, String nickName) {// SDK游客绑定的結果处理
+                switch (state) {
+                    case Constants.SDK_STATE_SUCCESS:// 游客绑定成功后的处理
+                        break;
+                    case Constants.SDK_STATE_CANCEL:// 游客绑定取消后的处理
+                        break;
+                    case Constants.SDK_STATE_ERROR:// 游客绑定失败后的处理
+                        break;
+                }
+            }
+
+            @Override
+            public void onHandleTokenFailure() {// SDK登录token失效的结果处理（需要游戏实现返回到登录页面的逻辑）
+
+            }
+
+            @Override
+            public void onHandlePay(int state, String eglsOrderInfo) {// SDK支付的結果处理
+                switch (state) {
+                    case Constants.SDK_STATE_SUCCESS:// 支付完成后的处理（仅表示客户端支付操作完成，最终要以服务器的通知为准）
+                        break;
+                    case Constants.SDK_STATE_CANCEL:// 支付取消后的处理
+                        break;
+                    case Constants.SDK_STATE_ERROR:// 支付失败后的处理
+                        break;
+                }
+            }
+
+            @Override
+            public void onHandleShare(int state, int type, String message) {// SDK分享的結果处理
+                switch (state) {
+                    case Constants.SDK_STATE_SUCCESS:// 分享成功后的处理
+                        // 当type为Constants.TYPE_SHARE_FACEBOOK时，表示Facebook分享
+                        // 当type为Constants.TYPE_SHARE_LINE时，表示LINE分享
+                        break;
+                    case Constants.SDK_STATE_CANCEL:// 分享失成功的处理
+                        break;
+                    case Constants.SDK_STATE_ERROR:// 分享失败后的处理
+                        break;
+                }
+            }
+
+            @Override
+            public void onHandleExit(boolean isExit) {// SDK退出游戏的結果处理（该方法只针对游戏调用SDK的"AGPManager.eglsExit()"接口的响应）
+                if (isExit) {
+                    // 玩家选择退出后的处理
+                } else {
+                    // 玩家选择继续游戏后的处理
                 }
             }
         });
@@ -64,59 +141,16 @@ public class MainActivity extends Activity {
     }
 
     private void requestLogin() {
-        int loginMode = Constants.MODE_LOGIN_COMMON;
-        AGPManager.eglsLogin(loginMode, new AGPLoginProcessListener() {
-
-            @Override
-            public void onLoginProcess(int action, String token, String uid, String accountType, String nickName) {
-
-            }
-
-            @Override
-            public void onLoginCancel() {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void onTokenFailure() {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void onAgreement(boolean isAgree) {
-
-            }
-        });
+        AGPManager.eglsLogin(Constants.MODE_LOGIN_AUTO);
     }
 
     private void requestPurchase() {
         String amount = "0.0";
         String productId = "";
         String productName = "";
-        String cpOrderId = "";
+        String cpOrderInfo = "";
         String flag = "";
-        AGPManager.eglsPay(amount, productId, productName, cpOrderId, flag, new AGPClientPayProcessListener() {
-
-            @Override
-            public void onClientPayFinish(String eglsOrderId) {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void onClientPayError() {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void onClientPayCancel() {
-                // TODO Auto-generated method stub
-
-            }
-        });
+        AGPManager.eglsPay(amount, productId, productName, cpOrderInfo, flag);
     }
 
     private void onEnterGame() {
--- egls-android-game-sdk-4/sdk/EglsSdkDemo/src/main/java/com/egls/demo/MainH5Activity.java
+++ egls-android-game-sdk-4/sdk/EglsSdkDemo/src/main/java/com/egls/demo/MainH5Activity.java
@@ -6,9 +6,8 @@ import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 
 import com.egls.platform.components.AGPManager;
-import com.egls.platform.interfaces.AGPClientPayProcessListener;
-import com.egls.platform.interfaces.AGPInitProcessListener;
-import com.egls.platform.interfaces.AGPLoginProcessListener;
+import com.egls.support.base.Constants;
+import com.egls.support.interfaces.SDKActionHandler;
 import com.egls.support.utils.AppUtil;
 
 import org.json.JSONException;
@@ -16,15 +15,125 @@ import org.json.JSONObject;
 
 public class MainH5Activity extends Activity {
 
+    private String jsLoginCallbackMethodName = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AGPManager.initSDK(this, AppUtil.getVersionName(this), true, new AGPInitProcessListener() {// SDK初始化回调
+        AGPManager.initSDK(this, AppUtil.getVersionName(this), new SDKActionHandler() {
 
             @Override
-            public void onInitProcess(int code, String msg) {
-                if (code == 0) {// 当SDK初始化成功后再做后续的事情
+            public void onHandleInit(int state, String message) {// SDK初始化的結果处理
+                if (state == Constants.SDK_STATE_SUCCESS) {
+                    // 初始化成功后的处理
+                } else {
+                    // 初始化失败后的处理
+                }
+            }
 
+            @Override
+            public void onHandleAgreement(boolean isAgree) {// SDK是否同意用户协议的結果处理
+                if (isAgree) {
+                    // 玩家选择同意协议后的处理
+                } else {
+                    // 玩家选择不同意协议后的处理
+                }
+            }
+
+            @Override
+            public void onHandleLogin(int state, String token, String uid, String accountType, String nickName) {// SDK登录的結果处理
+                switch (state) {
+                    case Constants.SDK_STATE_SUCCESS:// 登录成功后的处理
+                        try {
+                            JSONObject json = new JSONObject();
+                            json.put("action", Constants.SDK_STATE_SUCCESS);
+                            json.put("token", token);
+                            json.put("uid", uid);
+                            json.put("accountType", accountType);
+                            json.put("nickName", nickName);
+                            AGPManager.callJSMethodWithJsonStr(jsLoginCallbackMethodName, json.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case Constants.SDK_STATE_CANCEL:// 登录取消后的处理
+                        try {
+                            JSONObject json = new JSONObject();
+                            json.put("action", Constants.SDK_STATE_CANCEL);
+                            json.put("token", "");
+                            json.put("uid", "");
+                            json.put("accountType", "");
+                            json.put("nickName", "");
+                            AGPManager.callJSMethodWithJsonStr(jsLoginCallbackMethodName, json.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case Constants.SDK_STATE_ERROR:// 登录失败后的处理
+                        try {
+                            JSONObject json = new JSONObject();
+                            json.put("action", 3);
+                            json.put("token", "");
+                            json.put("uid", "");
+                            json.put("accountType", "");
+                            json.put("nickName", "");
+                            AGPManager.callJSMethodWithJsonStr(jsLoginCallbackMethodName, json.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                }
+            }
+
+            @Override
+            public void onHandleGuestBind(int state, String accountType, String nickName) {// SDK游客绑定的結果处理
+                switch (state) {
+                    case Constants.SDK_STATE_SUCCESS:// 游客绑定成功后的处理
+                        break;
+                    case Constants.SDK_STATE_CANCEL:// 游客绑定取消后的处理
+                        break;
+                    case Constants.SDK_STATE_ERROR:// 游客绑定失败后的处理
+                        break;
+                }
+            }
+
+            @Override
+            public void onHandleTokenFailure() {// SDK登录token失效的结果处理（需要游戏实现返回到登录页面的逻辑）
+
+            }
+
+            @Override
+            public void onHandlePay(int state, String eglsOrderInfo) {// SDK支付的結果处理
+                switch (state) {
+                    case Constants.SDK_STATE_SUCCESS:// 支付完成后的处理（仅表示客户端支付操作完成，最终要以服务器的通知为准）
+                        break;
+                    case Constants.SDK_STATE_CANCEL:// 支付取消后的处理
+                        break;
+                    case Constants.SDK_STATE_ERROR:// 支付失败后的处理
+                        break;
+                }
+            }
+
+            @Override
+            public void onHandleShare(int state, int type, String message) {// SDK分享的結果处理
+                switch (state) {
+                    case Constants.SDK_STATE_SUCCESS:// 分享成功后的处理
+                        // 当type为Constants.TYPE_SHARE_FACEBOOK时，表示Facebook分享
+                        // 当type为Constants.TYPE_SHARE_LINE时，表示LINE分享
+                        break;
+                    case Constants.SDK_STATE_CANCEL:// 分享失成功的处理
+                        break;
+                    case Constants.SDK_STATE_ERROR:// 分享失败后的处理
+                        break;
+                }
+            }
+
+            @Override
+            public void onHandleExit(boolean isExit) {// SDK退出游戏的結果处理（该方法只针对游戏调用SDK的"AGPManager.eglsExit()"接口的响应）
+                if (isExit) {
+                    // 玩家选择退出后的处理
+                } else {
+                    // 玩家选择继续游戏后的处理
                 }
             }
         });
@@ -68,6 +177,7 @@ public class MainH5Activity extends Activity {
 
     /**
      * H5与Android原生交互核心接口
+     *
      * @param jsonStr
      */
     @JavascriptInterface
@@ -86,7 +196,7 @@ public class MainH5Activity extends Activity {
                     String productName = json.optString("productName", "");
                     String cpOrderId = json.optString("cpOrderId", "");
                     requestPurchase(amount, productId, productName, cpOrderId);
-                }else if (method.equals("onEnterGame")) {// 进入游戏后需调用该接口
+                } else if (method.equals("onEnterGame")) {// 进入游戏后需调用该接口
                     onEnterGame();
                 }
             }
@@ -96,78 +206,12 @@ public class MainH5Activity extends Activity {
     }
 
     private void requestLogin(final int loginMode, final String callback) {
-        AGPManager.eglsLogin(loginMode, new AGPLoginProcessListener() {
-
-            @Override
-            public void onLoginProcess(int action, String token, String uid, String accountType, String nickName) {
-                try {
-                    JSONObject json = new JSONObject();
-                    json.put("action", action);
-                    json.put("token", token);
-                    json.put("uid", uid);
-                    json.put("accountType", accountType);
-                    json.put("nickName", nickName);
-                    AGPManager.callJSMethodWithJsonStr(callback, json.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onLoginCancel() {
-                try {
-                    JSONObject json = new JSONObject();
-                    json.put("action", 1);
-                    json.put("token", "");
-                    json.put("uid", "");
-                    json.put("accountType", "");
-                    json.put("nickName", "");
-                    AGPManager.callJSMethodWithJsonStr(callback, json.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onTokenFailure() {
-                try {
-                    JSONObject json = new JSONObject();
-                    json.put("action", 3);
-                    json.put("token", "");
-                    json.put("uid", "");
-                    json.put("accountType", "");
-                    json.put("nickName", "");
-                    AGPManager.callJSMethodWithJsonStr(callback, json.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onAgreement(boolean isAgree) {
-
-            }
-        });
+        jsLoginCallbackMethodName = callback;
+        AGPManager.eglsLogin(loginMode);
     }
 
     private void requestPurchase(final String amount, final String productId, final String productName, final String cpOrderId) {
-        AGPManager.eglsPay(amount, productId, productName, cpOrderId, "", new AGPClientPayProcessListener() {
-
-            @Override
-            public void onClientPayFinish(String eglsOrderId) {
-
-            }
-
-            @Override
-            public void onClientPayError() {
-
-            }
-
-            @Override
-            public void onClientPayCancel() {
-
-            }
-        });
+        AGPManager.eglsPay(amount, productId, productName, cpOrderId, "");
     }
 
     private void onEnterGame() {
